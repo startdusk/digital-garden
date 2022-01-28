546fcd1dcd8dc3f8f65139601489f6f45bf93988
startdusk <522383345@qq.com>
2022-01-28 22:49:06 +0800

    add(*): add testing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,31 @@
 use color_eyre::eyre::Result;
+use structopt::StructOpt;
+
+/// A CLI for the growing and curation of a digital garden
+///
+/// Visit https://www.rustadventure.rs/garden for more!
+#[derive(StructOpt, Debug)]
+struct Opt {
+    #[structopt(subcommand)]
+    cmd: Command,
+}
+
+#[derive(StructOpt, Debug)]
+enum Command {
+    /// write something in your garden
+    ///
+    /// This command will open your $EDITOR, wait for you
+    /// to write something, and then save the file to your
+    /// garden
+    Write {
+        #[structopt(short, long)]
+        title: Option<String>,
+    },
+}
+
 fn main() -> Result<()> {
     color_eyre::install()?;
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    dbg!(opt);
     todo!();
 }
